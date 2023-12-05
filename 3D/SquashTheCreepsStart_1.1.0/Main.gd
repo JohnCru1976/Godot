43924e076c3790450f826388b13d272a2acbad88
JohnCru1976 <94476964+JohnCru1976@users.noreply.github.com>
2023-12-05 09:25:02 +0100

    Finished Game

--- 3D/SquashTheCreepsStart_1.1.0/Main.gd
+++ 3D/SquashTheCreepsStart_1.1.0/Main.gd
@@ -2,6 +2,14 @@ extends Node
 
 @export var mob_scene: PackedScene
 
+func _ready():
+	$UserInterface/Retry.hide()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
+
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
@@ -15,9 +23,13 @@ func _on_mob_timer_timeout():
 	var player_position = $Player.position
 	mob.initialize(mob_spawn_location.position, player_position)
 
+	# We connect the mob to the score label to update the score upon squashing one.
+	mob.squashed.connect($UserInterface/ScoreLabel._on_mob_squashed.bind())
+
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
 
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
